65b42b5779d84bb22c9591acafdf1877f30ed2bb
C1326068 Jose Guilherme Santos Campos <c1326068@bb.com.br>
2022-05-30 15:33:03 -0300

    Initial Commit

---
+++ Shared/todolistApp.swift
@@ -0,0 +1,17 @@
+//
+//  todolistApp.swift
+//  Shared
+//
+//  Created by Jose campos on 30/05/22.
+//
+
+import SwiftUI
+
+@main
+struct todolistApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
